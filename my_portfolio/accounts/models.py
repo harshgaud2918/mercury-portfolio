0a344b94776615f1f1f54af400778e0bf9ea7622
Mercury_80 <harsh.gaud18@gmail.com>
2021-09-15 16:40:06 +0530

    changed model

--- my_portfolio/accounts/models.py
+++ my_portfolio/accounts/models.py
@@ -1,28 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
 
 class Project(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    body = models.TextField(null=True, blank=True)
-    code_link = models.TextField(null=True, blank=True)
-    other_link = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=200,null=True, blank=True,default='')
+    description = models.TextField(null=True, blank=True,default='')
+    thumbnail = models.TextField(null=True, blank=True,default='')
+    body = models.TextField(null=True, blank=True,default='')
+    code_link = models.TextField(null=True, blank=True,default='')
+    other_link = models.TextField(null=True, blank=True,default='')
 
 
     def __str__(self):
         return self.name
 
 class Experience(models.Model):
-    organization = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    body = models.TextField(null=True, blank=True)
-    start_date = models.DateField(null = True,blank = True)
-    end_date = models.DateField(null = True,blank = True)
-    code_link = models.TextField(null=True, blank=True)
-    other_link = models.TextField(null=True, blank=True)
+    organization = models.CharField(max_length=200,null=True, blank=True,default='')
+    description = models.TextField(null=True, blank=True,default='')
+    thumbnail = models.TextField(null=True, blank=True,default='')
+    body = models.TextField(null=True, blank=True,default='')
+    start_date = models.CharField(max_length=200,null=True, blank=True,default='')
+    end_date = models.CharField(max_length=200,null=True, blank=True,default='')
+    code_link = models.TextField(null=True, blank=True,default='')
+    other_link = models.TextField(null=True, blank=True,default='')
 
 
     def __str__(self):
